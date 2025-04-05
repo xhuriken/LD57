df839dffd5ff00c90eee1faa626f2726e9438d71
xhuriken <celestin@honvault.Com>
2025-04-05 22:49:12 +0200

    Add Debug Mode for menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    public bool DebugMode = false;
+
     public bool isDragging = false;
     public bool menuShown = false;
     public bool isStarted = false;
+
 
     private CameraController camControl;
     private void Awake()
@@ -22,12 +26,21 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-            camControl = Camera.main.GetComponent<CameraController>();
+        camControl = Camera.main.GetComponent<CameraController>();
+        if (!DebugMode)
+        {
             camControl.enabled = false;
+        }
+        else
+        {
+            Camera.main.transform.position = new Vector3(0, 0, -10);
+        }
     }
 
     private void Update()
     {
+        if(DebugMode)
+            return;
         if (menuShown && camControl.enabled)
         {
             camControl.enabled = false;
